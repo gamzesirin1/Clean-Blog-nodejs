97744ca2b2a76b8825ff301bd3176991c2a717da
Gamze Şirin <117711013+Gamzesrn@users.noreply.github.com>
2023-09-12 15:18:02 +0300

    static files uploaded.

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 const express = require('express');
+const esj = require('ejs');
+const path = require('path');
 const app = express();
+
+const myLogger = (req, res, next) => {
+  console.log('Middleware Log 1');
+  next();
+};
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(myLogger);
 app.get('/', (req, res) => {
   const blog = { id: 1, title: 'Blog title', description: 'Blog description' };
   res.send(blog);
+});
+const port = 5000;
+app.listen(port, () => {
+  console.log(`Sunucu ${port} portunda başlatıldı.`);
 });

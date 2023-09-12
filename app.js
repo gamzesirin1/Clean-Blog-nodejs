af21cfb055b3cd4df7416a784b6935d4adc7de3c
Gamze Şirin <117711013+Gamzesrn@users.noreply.github.com>
2023-09-12 16:17:28 +0300

    created views and partials files.

--- app.js
+++ app.js
@@ -10,10 +10,18 @@ const myLogger = (req, res, next) => {
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(myLogger);
+
 app.get('/', (req, res) => {
-  const blog = { id: 1, title: 'Blog title', description: 'Blog description' };
-  res.send(blog);
+  res.render('index');
 });
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/add_post', (req, res) => {
+  res.render('add_post');
+});
+
 const port = 5000;
 app.listen(port, () => {
   console.log(`Sunucu ${port} portunda başlatıldı.`);

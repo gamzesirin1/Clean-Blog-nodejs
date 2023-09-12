c1674d807f7524e14ee69f662607e6099b93f378
Gamze Şirin <117711013+Gamzesrn@users.noreply.github.com>
2023-09-12 13:49:12 +0300

    workspace created

---
+++ app.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => {
+  const blog = { id: 1, title: 'Blog title', description: 'Blog description' };
+  res.send(blog);
+});
